d41cc71aeb644bdee7a0de5967ddc64672682a95
ktcaudill99 <ktcaudill99@gmail.com>
2023-07-31 14:02:52 -0400

    working on reports

--- src/schedule/ReportController.java
+++ src/schedule/ReportController.java
@@ -12,20 +12,17 @@ import java.io.IOException;
 import java.sql.*;
 import javafx.event.ActionEvent;
 
+
 public class ReportController {
     @FXML
-    private TextArea reportArea;
+    private TextArea reportArea1;
+    @FXML
+    private TextArea reportArea2;
 
     @FXML
-    private void generateReports() {
-        reportArea.clear();
-        generateReport1();
-        generateReport2();
-    }
-
     private void generateReport1() {
+        reportArea1.clear();
         String query = "SELECT COUNT(*), Type, MONTH(Start) as Month FROM client_schedule.appointments GROUP BY Type, Month ORDER BY Month, Type";
-
         try {
             Statement statement = ConnectDB.conn.createStatement();
             ResultSet results = statement.executeQuery(query);
@@ -33,15 +30,15 @@ public class ReportController {
                 int count = results.getInt(1);
                 String type = results.getString(2);
                 int month = results.getInt(3);
-
-                reportArea.appendText("Month: " + month + ", Type: " + type + ", Count: " + count + "\n");
+                reportArea1.appendText("Month: " + month + ", Type: " + type + ", Count: " + count + "\n");
             }
         } catch (SQLException e) {
-            reportArea.appendText("Error generating report: " + e.getMessage() + "\n");
+            reportArea1.appendText("Error generating report: " + e.getMessage() + "\n");
         }
     }
-
+    @FXML
     private void generateReport2() {
+        reportArea2.clear();
         String query = "SELECT Contact_ID FROM client_schedule.contacts";
         try {
             Statement statement = ConnectDB.conn.createStatement();
@@ -51,18 +48,18 @@ public class ReportController {
                 generateContactSchedule(contactId);
             }
         } catch (SQLException e) {
-            reportArea.appendText("Error generating report: " + e.getMessage() + "\n");
+            reportArea2.appendText("Error generating report: " + e.getMessage() + "\n");
         }
     }
 
+
     private void generateContactSchedule(int contactId) {
         String query = "SELECT Appointment_ID, Title, Type, Description, Start, End, Customer_ID FROM client_schedule.appointments WHERE Contact_ID = ? ORDER BY Start";
-
         try {
             PreparedStatement statement = ConnectDB.conn.prepareStatement(query);
             statement.setInt(1, contactId);
             ResultSet results = statement.executeQuery();
-            reportArea.appendText("Schedule for Contact " + contactId + ":\n");
+            reportArea2.appendText("Schedule for Contact " + contactId + ":\n");
             while (results.next()) {
                 int appointmentId = results.getInt(1);
                 String title = results.getString(2);
@@ -72,10 +69,10 @@ public class ReportController {
                 Timestamp end = results.getTimestamp(6);
                 int customerId = results.getInt(7);
 
-                reportArea.appendText("Appointment ID: " + appointmentId + ", Title: " + title + ", Type: " + type + ", Description: " + description + ", Start: " + start + ", End: " + end + ", Customer ID: " + customerId + "\n");
+                reportArea2.appendText("Appointment ID: " + appointmentId + ", Title: " + title + ", Type: " + type + ", Description: " + description + ", Start: " + start + ", End: " + end + ", Customer ID: " + customerId + "\n");
             }
         } catch (SQLException e) {
-            reportArea.appendText("Error generating schedule: " + e.getMessage() + "\n");
+            reportArea2.appendText("Error generating schedule: " + e.getMessage() + "\n");
         }
     }
 
